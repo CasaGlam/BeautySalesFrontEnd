fca559e2d88667a825f7161e78755b04655410b5
SxntiBG <zapataospinasantiago8@gmail.com>
2024-06-22 14:23:15 -0500

    despliegue

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ajusta esto si tu aplicación está en un subdirectorio
+  base: '/', // Ajusta esto si la aplicación está en un subdirectorio
   build: {
-    outDir: 'dist', // Cambia esto si necesitas una carpeta de salida diferente
+    outDir: 'dist',
     rollupOptions: {
       output: {
         entryFileNames: '[name].[hash].js',
         chunkFileNames: '[name].[hash].js',
         assetFileNames: '[name].[hash].[ext]',
-      }
-    }
-  },
-  server: {
-    // Configuraciones para el entorno de desarrollo
-    port: 3000,
-    open: true, // Abre el navegador automáticamente
-    proxy: {
-      // '/api': 'http://localhost:5000', // Ejemplo de configuración de proxy
+      },
     },
-  }
+  },
 });
